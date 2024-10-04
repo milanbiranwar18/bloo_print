a7c6af3c26a75496633000323821348db5e8bffa
Milan Biranwar <milanbiranwar06@gmail.com>
2024-10-05 02:48:36 +0530

    Add user registraion, login, logout api code

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    mobile_num = models.IntegerField()
+    location = models.CharField(max_length=100)
